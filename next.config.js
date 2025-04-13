c79e659d2be6354ef120b2239c272bd7070049a3
fabiovitorino97 <fabiiovr@gmail.com>
2025-04-13 11:33:50 -0300

    Config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Isso gera arquivos estáticos
+  basePath: '/Calculadora-Next',
+  images: {
+    unoptimized: true, // Necessário para deploy estático
+  },
+  trailingSlash: true, // Recomendado para GitHub Pages
 }
+
+module.exports = nextConfig
